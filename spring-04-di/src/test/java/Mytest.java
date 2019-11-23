15870d703cdc9a933d015dd0a9dce74264f5acbd
Leo <364779323@qq.com>
2019-11-23 21:39:26 +0800

    null

--- spring-04-di/src/test/java/Mytest.java
+++ spring-04-di/src/test/java/Mytest.java
@@ -6,6 +6,6 @@ public class Mytest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student) context.getBean("student");
-        System.out.println(student);
+        System.out.println(student.toString());
     }
 }
